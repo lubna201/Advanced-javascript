5bf5d7918ff255f687780e9837d674eeeb9690ab
lubna201 <lubnasazida@gmail.com>
2021-02-02 01:57:51 +0600

    null vs undifined

---
+++ null-undifined.js
@@ -0,0 +1,25 @@
+// let pakhi;
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+// const result=add(13,82);
+// console.log(result);
+
+// function add(num1,num2){
+//     console.log(num2);
+// }
+
+// const result=add(13);
+// console.log(result);
+
+// const premik={name:"smart dude",phone:878979843};
+// console.log(premik.gf);
+
+// let fun=undefined;
+// console.log(fun);
+
+//null
+let ages=[2,5,9];
+console.log(ages[11]);
+
+
