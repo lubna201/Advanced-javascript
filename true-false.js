a919243987f9909ac8c436457e1bdcc3d07e310e
lubna201 <lubnasazida@gmail.com>
2021-02-02 01:17:12 +0600

    true-false update

---
+++ true-false.js
@@ -0,0 +1,26 @@
+//0
+//""
+// undefine
+//null
+//false
+//NaN=not a number
+// these are the false values
+
+
+//'0' ," ",'false'
+
+// const age=4;
+// const name="solaiman";
+// const name="0";
+
+// let name=null;
+// let name=NaN;
+let name=[];
+console.log(name);
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
